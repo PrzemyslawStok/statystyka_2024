f58253f83509c7920b2afce5d0af46dc30c7bbd5
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2024-05-31 19:03:32 +0200

    requirements.txt

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import numpy as np
 from matplotlib import pyplot as plot
 
 if __name__ == '__main__':
-    x = np.linspace(0, 10, 5)
-    y = np.sin(x)
+    fig, ax = plot.subplots(2, 2, figsize=(10, 10), dpi=200)
 
-    plot.plot(x, y)
-    plot.show()
+    ax = ax.ravel()
 
-    x = np.random.random(1000000)
+    linspace_dim = [5, 10, 20, 100, 200, 500]
 
-    plot.hist(x, bins=100)
+    for i, dim in enumerate(linspace_dim):
+        x = np.linspace(-np.pi, np.pi, dim)
+        y = np.sin(x)
+
+        ax[0].plot(x, y, label=fr"$y=sin(x), dim={dim}$")
+
+    ax[0].set_xlabel('x')
+    ax[0].set_ylabel('y')
+    ax[0].legend()
+
     plot.show()
