4d401ce8406240cd00d7a91d14a27d2dbd3f5dfa
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2024-06-14 18:15:27 +0200

    requirements.txt

--- main.py
+++ main.py
@@ -18,4 +18,12 @@ if __name__ == '__main__':
     ax[0].set_ylabel('y')
     ax[0].legend()
 
+    height = np.random.randint(160, 200, 100)
+    height = np.append(height, np.random.randint(170, 175, 200))
+    np.random.shuffle(height)
+    x = np.arange(0, len(height))
+
+    ax[1].plot(x, height,label=fr"$wzrost$")
+    ax[2].hist(height, bins=20)
+
     plot.show()
